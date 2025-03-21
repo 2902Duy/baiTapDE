8d1bdbc1e68866ef1d2ea426b2d13ba81f4d9815
Dyu2902 <tduy29.2k4@gmail.com>
2025-03-21 07:59:39 +0700

    format_date

--- sparkRDD/SparkSQL/baitap.py
+++ sparkRDD/SparkSQL/baitap.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import StructType
-
+from datetime import datetime
 
 sp = SparkSession.builder \
     .appName("DE-ETL-102")\
@@ -13,22 +13,34 @@ sp = SparkSession.builder \
 
 data = [("11/12/2025",),("27/02.2014",),("2023.01.09",),("28-12-2005",)]
 
+df = sp.createDataFrame(data,["date_raw"])
 
-def slip_date(date):
-    if(date[2].isdigit()):
-        return int(date[8:]), int(date[5:7]),int(date[:4])
-    else:
-        return int(date[:2]),int(date[3:5]),int(date[6:])
-    ## return day month year
+@udf(StringType())
+def format_date(date_str):
+    date_formats = [
+        "%d-%m-%Y",
+        "%Y-%m-%d",
+        "%d-%m-%y",
+    ]
+    clean_str = date_str.replace('.', '-').replace('/', '-')
+    for fmt in date_formats:
+        try:
+            parsed_date = datetime.strptime(clean_str, fmt)
+            return parsed_date.strftime("%Y-%m-%d")
+        except ValueError:
+            continue
 
-schema = StructType([
-    StructField("day", IntegerType(), True),
-    StructField("month", IntegerType(), True),
-    StructField("year", IntegerType(), True)
-])
-df = sp.createDataFrame(data, ["date"])
-slip_date_udf = udf(slip_date, schema)
-df_parsed = df.withColumn("dmy", slip_date_udf("date")) \
-              .select("date", "dmy.*")
+df_result = (df.withColumn(
+    "date_str", format_date("date_raw")
+).withColumn(
+    "date_format", to_date("date_str")
+).withColumn(
+    "day",day("date_format")
+).withColumn(
+    "month",month("date_format")
+).withColumn(
+    "year",year("date_format")
+)
+.select("date_raw", "date_format","day","month","year"))
 
-df_parsed.show(truncate=False)
+df_result.show(truncate=False)
